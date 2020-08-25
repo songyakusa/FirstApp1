9a8a01fd3effe92cec369d2a88315ae0737f8c99
CHARAN <songyakusa@gmail.com>
2020-08-26 03:29:02 +0700

    crate Aboutme

--- app/src/main/java/com/example/firstapp1/MainActivity.kt
+++ app/src/main/java/com/example/firstapp1/MainActivity.kt
@@ -1,11 +1,87 @@
-package com.example.firstapp1
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.firstapp1.R
+import com.example.firstapp1.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var drawerLayout: DrawerLayout
+
+    private lateinit var toggle: ActionBarDrawerToggle
+
+    private lateinit var nav_view: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        drawerLayout = binding.drawerLayout
+        nav_view = binding.navView
+        nav_view.setNavigationItemSelectedListener(this)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+
+        drawerLayout.addDrawerListener(toggle)
+
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+    // click  back  to activity
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+
+        } else {
+            super.onBackPressed()
+        }
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+
+    }
+    // fun click menu  aboutme and contact
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.aboutme -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(
+                        R.id.fragment_contain
+                        , Aboutme()
+
+                    )
+                    .commit()
+            }
+            R.id.contact -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(
+                        R.id.fragment_contain
+                        , Contact()
+
+                    )
+                    .commit()
+            }
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    // click teb show menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/firstapp1/aboutme/Aboutme.kt
@@ -0,0 +1,16 @@
+package com.example.firstapp1.aboutme
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.firstapp1.R
+
+class Aboutme : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+}
